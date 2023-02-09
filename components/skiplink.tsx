baf89a76cc2226b35618ce8e023e8b555a371fe3
Carol <carolandrades11@gmail.com>
2023-02-09 20:20:42 +0000

    feat: create SkipLink function

---
+++ components/skiplink.tsx
@@ -0,0 +1,11 @@
+// https://prismic.io/blog/nextjs-accessibility
+
+const SkipLink = () => {
+    return (
+      <a href="#main" className="skip-link sr-only">
+        Skip to content
+      </a>
+    )
+  }
+  
+  export default SkipLink;
