514a3277391191478f1a4435435f3e5aff434ff1
Carol <carolandrades11@gmail.com>
2023-02-20 22:01:06 +0000

    feat(toggleTheme): create button toggle theme and update Tailwind config file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,64 +1,57 @@
 'use client';
 
-import clsx from 'clsx';
-import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import Logo from './logo';
+// import clsx from 'clsx';
+// import Link from 'next/link';
+// import { usePathname } from 'next/navigation';
+// import { navLinks } from '@/lib/info';
+import dynamic from 'next/dynamic';
+import { Logo } from './logo';
 
-const navLinks = {
-    '/': {
-        name: 'home'
-    },
-    '/about': {
-        name: 'about'
-    },
-    '/projects': {
-        name: 'projects'
-    },
-    '/guestbook': {
-        name: 'guestbook'
-    },
-};
+const ToggleTheme = dynamic(async () => {
+let myModule = await import('../components/toggleTheme')
+return myModule.ToggleTheme;
+}, { ssr: false });
 
 
+// function LinkItem(isActive:boolean, name: string, path: string) {
+//     return <>
+//         {isActive && <span className="sr-only">Current page: </span>}
+//         <Link
+//         {...(isActive ? {"aria-current":"page"} : {})}
+//         href={path}
+//         className={clsx(
+//             "relative flex justify-center items-center transition-all hover:text-gray-600 dark:hover:text-neutral-200 py-1 px-2.5", {
+//                 'text-neutral-600': !isActive,
+//                 'text-black font-bold': isActive,
+//             }
+//         )}                       
+//         >
+//             {name}
+//         </Link>
+//     </>
+// }
 
-function Navbar() {
-    let pathname = usePathname(); 
+export const Navbar = () => {
+    // let pathname = usePathname(); 
 
   return (
-    <header className="flex flex-col sticky top-0 md:flex-row justify-between">
+    <header className="flex justify-between items-center mb-20">
         <Logo />
-        <nav id="navigation">
-            <ul role="list" className="flex">
-                    
-                    {Object.entries(navLinks).map(([path, { name }]) => {
-                        const isActive = path === pathname;
 
-                        return (
-                            <li key={path} className="relative rounded-xl overflow-hidden ">
-                                <>
-                                    {isActive && <span className="sr-only">Current page: </span>}
-                                    <Link
-                                    {...(isActive ? {"aria-current":"page"} : {})}
-                                    href={path}
-                                    className={clsx(
-                                        "relative flex justify-center items-center transition-all hover:text-white dark:hover:text-neutral-200 py-[5px] px-[10px]", {
-                                            'text-neutral-500': !isActive,
-                                            'navLinkActive': isActive,
-                                        }
-                                    )}                       
-                                    >
-                                        {name}
-                                    </Link>
-                                </>
-                            </li>
-                        );
-                    })}
+        <ToggleTheme />
+
+        {/* <nav id="navigation" className="py-2">
+            <ul role="list" className="flex justify-between">
+                {Object.entries(navLinks).map(([path, { name }]) => {
+                    const isActive = path === pathname;
+                    return (
+                        <li key={path} className="relative flex justify-center items-center rounded-xl overflow-hidden hover:shadow-md shadow-cyan-500/50">
+                            {LinkItem(isActive, name, path)}
+                        </li>
+                    );
+                })}
             </ul>
-        </nav>
+        </nav> */}
     </header>
   )
-}
-
-
-export default Navbar;
+};
