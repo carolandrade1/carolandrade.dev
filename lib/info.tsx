b786f78c6bf9858182f66ac72f4f68cdbd9875eb
Carol <carolandrades11@gmail.com>
2023-02-22 23:08:35 +0000

    feat: create file with all the info of the website

---
+++ lib/info.tsx
@@ -0,0 +1,40 @@
+export const info = {
+    "id": "01",
+    "name": "Home",
+    "photoUrl": "/profilePhoto.jpeg",
+    "photoAlt": "test",
+    "h1": "Carol Andrade Santos",
+    "description": "Front end developer",
+    "paragr00": "Hello, I'm Carol. I'm currently living in Canada and looking for my first opportunity here. Despite having a bachelor in civil engineer, I made my career transition in 2021.",
+    "h2": "Projects",
+    "paragr01": "Some of my projects that I did in the past.",
+    "gridProject": [
+        {
+            "cardId": "01",
+            "title": "Google Clone",
+            "description": "Google clone using Next.js and Tailwind",
+            "link": "https://google-clone-livid-zeta.vercel.app/pt-BR",
+            "buttonText": "See live",
+            "primaryBackground": "bg-violet-400",
+            "secondaryBackground": "from-blue-700 to-blue-700"
+        },
+        {
+            "cardId": "02",
+            "title": "Projects JavaScript",
+            "description": "Multiple projects manipulating the DOM",
+            "link": "https://carolandrade1.github.io/Javascript_Projects/",
+            "buttonText": "See live",
+            "primaryBackground": "bg-violet-500",
+            "secondaryBackground": "from-indigo-700 to-indigo-700"
+        },
+        {
+            "cardId": "03",
+            "title": "Responsive Layouts",
+            "description": "10 Modern CSS single-line styling",
+            "link": "https://github.com/carolandrade1/Responsive_Layout",
+            "buttonText": "See Live",
+            "primaryBackground": "bg-violet-500",
+            "secondaryBackground": "from-indigo-700 to-indigo-700"
+        }
+    ]
+}
