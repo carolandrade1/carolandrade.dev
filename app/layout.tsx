419c593dbf87433b2baf16b6100a5fcadc550529
Carol <carolandrades11@gmail.com>
2023-02-22 23:03:11 +0000

    feat: add metadata and script for toggleTheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import Navbar from '@/components/navbar'
-import SkipLink from '@/components/skiplink'
-import { Ibarra_Real_Nova } from '@next/font/google'
-import clsx from 'clsx'
-import './globals.css'
+import './globals.css';
+import Script from 'next/script';
+import { Ibarra_Real_Nova } from '@next/font/google';
+import { Footer } from '@/components/footer';
+import { Navbar } from '@/components/navbar';
+import { SkipLink } from '@/components/skiplink';
 
 const ibarra = Ibarra_Real_Nova({
   subsets: ['latin'],
@@ -10,30 +11,65 @@ const ibarra = Ibarra_Real_Nova({
   display: 'swap',
 })
 
+export const metadata = {
+  title: {
+    default: 'Carol Santos',
+    template: '%s | Carol Santos',
+  },
+  description: 'Portfolio',
+  generator: 'Next.js',
+  applicationName: 'Portfolio',
+  referrer: 'origin-when-crossorigin',
+  keywords: ['Portfolio', 'Next.js', 'React'],
+  themeColor: 'tomato',
+  colorScheme: 'dark',
+  creator: 'Carol Andrade',
+  publisher: 'Carol Andrade',
+}
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className={clsx(
-      "text-black bg-white dark:text-white dark:bg-[#111010]", ibarra.className
-    )} >
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
 
-      <body className="antialiased max-w-4xl mb-40 flex flex-col mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
+  const setInitialTheme = `
+    function getUserPreference() {
+      if(window.localStorage.getItem('theme')) {
+        return window.localStorage.getItem('theme')
+      }
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light'
+    }
+    document.body.dataset.theme = getUserPreference();
+  `;
+
+  return (
+    <html lang="en" >
+
+      <body className="text-[#18181B] bg-white dark:text-white dark:bg-[#18181B] antialiased m-auto max-w-4xl p-6 flex flex-col height">
+        <Script
+          id="data-theme"
+          dangerouslySetInnerHTML={{ __html: setInitialTheme }}
+        />
+
+        {/* <div className="z-[-1] pointer-events-none absolute top-0 left-0 h-72 w-full md:h-80 lg:h-96">
+          <div className="content h-full">
+            <div className="    bg-gradient-to-b from-purple-200 w-full h-full"></div>
+          </div>
+        </div> */}
+
         <SkipLink />
 
         <Navbar />
         
-        <main id="main" className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
+        <main id="main" className="flex-auto min-w-0 md:flex flex-col px-2 md:px-0">
           {children}
         </main>
       
+        <Footer />
       </body>
     </html>
   )
