d185f81c31e0941746f19a2ca2152bcf972a0cc4
Carol <carolandrades11@gmail.com>
2023-02-22 17:18:07 +0000

    feat: create Home function

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { ProjectGallery } from "@/components/projects";
+import Image from "next/image";
+import { info } from "../lib/info";
 
 export default function Home() {
   return (
-    <main>
-
-          <h1 className="text-3xl font-bold underline">Hello World!</h1>
-       
-    </main>
+    <>
+    <section className="mb-20">
+      <div className="mb-5">
+        <Image
+          src={info.photoUrl}
+          alt={info.photoAlt}
+          width={80}
+          height={80}
+          className="rounded-lg"
+        />
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">{info.h1}</h1>
+        <p className="mb-5 text-gray-500 dark:text-gray-400">{info.description}</p>
+        <p className="leading-8 max-w-xl">{info.paragr00}</p>
+      </div>
+    </section>
+    <section className="mb-16">
+      <div>
+        <h2 className="text-xl font-bold">{info.h2}</h2>
+        <p className="mb-5">{info.paragr01}</p>
+      </div>
+      <ProjectGallery />
+    </section>
+    </>
   )
 }
