a05e4cd6b6733b8c80cc233782b6c33d4285c1e9
Carol <carolandrades11@gmail.com>
2023-02-27 00:03:06 +0000

    refactor: adapt info file

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,58 +1,79 @@
 export const info = {
-    "id": "01",
-    "name": "Home",
-    "photoUrl": "/profilePhoto.jpeg",
-    "photoAlt": "test",
-    "introductionSection": {
-        "h1": "Carol Andrade Santos",
-        "role": "Front end developer ✨",
-        "description": "Hello 👋 I'm Carol, a Front End Developer from Brazil 🇧🇷. Now I currently live in Canada 🇨🇦 and I'm looking for my first job here.",
-        "contact": [
-            {
-                "contactId": "01",
-                "name": "Github",
-                "url": "https://github.com/carolandrade1",
-            },
-            {
-                "contactId": "02",
-                "name": "LinkedIn",
-                "url": "https://www.linkedin.com/in/carolandrade1/",
-            },
-            {
-                "contactId": "03",
-                "name": "Codepen",
-                "url": "https://codepen.io/carolandrade1",
-            },
-        ],
+  "/": {
+    name: "HomeDummy",
+    photoUrl: "/profilePhoto.jpeg",
+    photoAlt: "Picture of Carol Andrade",
+    introductionSection: {
+      h1: "Carol Andrade Santos",
+      role: "Front end developer ✨",
+      description:
+        "Hello 👋 I'm Carol, a Front End Developer from Brazil 🇧🇷. Now I currently live in Canada 🇨🇦 and I'm looking for my first job here.",
+      contact: [
+        {
+          contactId: "01",
+          name: "Github",
+          url: "https://github.com/carolandrade1",
+        },
+        {
+          contactId: "02",
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/carolandrade1/",
+        },
+        {
+          contactId: "03",
+          name: "Codepen",
+          url: "https://codepen.io/carolandrade1",
+        },
+      ],
     },
-    "projectSection": {
-        "h2": "Projects",
-        "description": "Here you can see some of my projects that I did in the past.",
-        "gridProject": [
-            {
-                "cardId": "01",
-                "title": "Google Clone",
-                "description": "Google clone using Next.js and Tailwind",
-                "link": "https://google-clone-livid-zeta.vercel.app/pt-BR",
-                "linkCode": "https://github.com/carolandrade1/GoogleClone",
-                "buttonText": "See live",
-            },
-            {
-                "cardId": "02",
-                "title": "Projects JavaScript",
-                "description": "Multiple projects manipulating the DOM",
-                "link": "https://carolandrade1.github.io/Javascript_Projects/",
-                "linkCode": "https://github.com/carolandrade1/Javascript_Projects",
-                "buttonText": "See live",
-            },
-            {
-                "cardId": "03",
-                "title": "Responsive Layouts",
-                "description": "10 Modern CSS single-line styling",
-                "link": "https://carolandrade1.github.io/Responsive_Layout/",
-                "linkCode": "https://github.com/carolandrade1/Responsive_Layout",
-                "buttonText": "See Live",
-            }
-        ],
+    projectSection: {
+      h2: "Projects",
+      description:
+        "Here you can see some of my projects that I did in the past.",
+      gridProject: [
+        {
+          cardId: "01",
+          title: "Google Clone",
+          description: "Google clone using Next.js and Tailwind",
+          link: "https://google-clone-livid-zeta.vercel.app/pt-BR",
+          linkCode: "https://github.com/carolandrade1/GoogleClone",
+          buttonText: "See live",
+        },
+        {
+          cardId: "02",
+          title: "Projects JavaScript",
+          description: "Multiple projects manipulating the DOM",
+          link: "https://carolandrade1.github.io/Javascript_Projects/",
+          linkCode: "https://github.com/carolandrade1/Javascript_Projects",
+          buttonText: "See live",
+        },
+        {
+          cardId: "03",
+          title: "Responsive Layouts",
+          description: "10 Modern CSS single-line styling",
+          link: "https://carolandrade1.github.io/Responsive_Layout/",
+          linkCode: "https://github.com/carolandrade1/Responsive_Layout",
+          buttonText: "See Live",
+        },
+      ],
     },
-}
+  },
+  "/library": {
+    name: "Library",
+    photoUrl: "",
+    photoAlt: "",
+    introductionSection: {
+      h1: "Library",
+      role: "Search in my personal library for articles that I read and use for reference",
+    },
+  },
+  "/guestbook": {
+    name: "Guestbook",
+    photoUrl: "",
+    photoAlt: "",
+    introductionSection: {
+      h1: "Guestbook",
+      role: "Leave a feedback, ... ",
+    },
+  },
+};
