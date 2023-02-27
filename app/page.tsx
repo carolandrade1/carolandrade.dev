f798ecde4661b18eb82bc055524b70200251162d
Carol <carolandrades11@gmail.com>
2023-02-27 00:14:17 +0000

    refactor: create supabase request

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import { IntroductionSection } from "@/components/introduction";
 import { ProjectSection } from "@/components/projects";
+import { supabase } from "@/lib/supabase";
+import { info } from "@/lib/info";
 
+export const revalidate = 60;
 
-export default function Home() {
+export async function getData() {
+  const { data } = await supabase.from('info').select().eq('id', '/');
+
+  return data ? data[0].page : info["/"];
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <>
       {/* <div className="z-[-1] pointer-events-none absolute top-0 left-0 h-52 w-full">
@@ -10,8 +20,8 @@ export default function Home() {
             <div className="bg-gradient-to-b from-violet-500/60 w-full h-full"></div>
           </div>
       </div> */}
-      <IntroductionSection />
-      <ProjectSection />
+      <IntroductionSection data={data} />
+      <ProjectSection data={data}/>
     </>
-  )
+  );
 }
