b9cff1d8ecae63586681aeae12b7bc5ce769d5e1
Carol <carolandrades11@gmail.com>
2023-03-01 00:35:40 +0000

    refactor: use @apply to render new class on buttonBg

--- components/buttonBg.tsx
+++ components/buttonBg.tsx
@@ -5,15 +5,15 @@ interface ButtonComponents {
 }
 
 export enum ButtonColor {
-    zinc = "bg-zinc-800 dark:bg-zinc-700 drop-shadow-md absolute inset-0 -z-10 w-full h-full rounded-lg origin-center group-hover:scale-110 group-hover:opacity-90 duration-200",
-    darkPurple = "bg-purple-600 drop-shadow-md absolute inset-0 -z-10 w-full h-full rounded-lg origin-center group-hover:scale-110 duration-200"
+    zinc = "bg-zinc-800 dark:bg-zinc-700 group-hover:opacity-90",
+    darkPurple = "bg-purple-600"
 }
 
 
 export function ButtonBg({color, text, href}:ButtonComponents) {
     return (
       <a href={href} className="group relative inline-block px-4 py-2 text-white" target="_blank" rel="noopener noreferrer">
-        <div className={`${color} ...`}></div>
+        <div className={`${color} ... btnBackground`}></div>
         {text}
       </a>
     )
