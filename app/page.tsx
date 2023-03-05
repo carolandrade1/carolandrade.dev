97e66e09cec7426dbc1fd6c5e5f175013e304246
Carol <carolandrades11@gmail.com>
2023-03-05 03:40:46 +0000

    refactor: change name file

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import { supabase } from "@/lib/supabase";
 import { info } from "@/lib/info";
 import { IntroductionSection } from "@/components/sections/introduction";
 import { ProjectSection } from "@/components/sections/projects";
-import { LibrarySection } from "@/components/sections/librarySection";
-
-export const revalidate = 60;
+import { LibrarySection } from "@/components/sections/library";
 
 async function getData() {
   const { data } = await supabase.from('info').select().eq('id', '/');
 
   return data ? data[0].page : info["/"];
 }
+
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   let data
