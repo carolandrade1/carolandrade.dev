1936debd20435239f24db0a03621d6cf57e6b7e2
Carol <carolandrades11@gmail.com>
2023-03-05 22:59:07 +0000

    refactor: style SkipLInk component

--- components/miscellaneous/skiplink.tsx
+++ components/miscellaneous/skiplink.tsx
@@ -1,6 +1,6 @@
 export const SkipLink = () => {
     return (
-      <a href="#main" className="skip-link sr-only">
+      <a href="#main" className="sr-only focus:not-sr-only focus:py-4 focus:px-6 focus:underline focus:border-2 focus:absolute focus:index-0 focus:bg-white dark:focus:bg-black">
         Skip to content
       </a>
     )
