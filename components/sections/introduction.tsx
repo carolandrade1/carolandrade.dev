8d29cf01cb42b36ed1815adfe030912292d2bf4e
Carol <carolandrades11@gmail.com>
2024-01-13 13:06:58 -0500

    feat: add animatedSparkles component on introduction section.

--- components/sections/introduction.tsx
+++ components/sections/introduction.tsx
@@ -1,47 +1,63 @@
-import Image from "next/image";
-import { DescriptionIcon, MessageIcon } from "@/lib/icons";
-import { ButtonBg } from "@/components/miscellaneous/buttonBg";
-import { ButtonColor, Contacts, Introduction } from "@/types/types";
+import Image from 'next/image';
+import { DescriptionIcon, MessageIcon } from '@/lib/icons';
+import { ButtonBg } from '@/components/miscellaneous/buttonBg';
+import { ButtonColor, Contacts, Introduction } from '@/types/types';
+import Sparkles from '@/components/miscellaneous/animatedSparkles';
 
-export function IntroductionSection({data}: any) {
-const introductionSection: Introduction = data.introductionSection;
+export function IntroductionSection({ data }: any) {
+  const introductionSection: Introduction = data.introductionSection;
 
   return (
-    <section className="mb-16 md:mb-28">
-        <div className="mb-2 md:mb-5">
-            <Image
-                priority
-                src={data.photoUrl}
-                alt={data.photoAlt}
-                width={100}
-                height={100}
-                className="rounded-lg w-[80px] h-[80px] md:w-[100px] md:h-[100px]" />
+    <section className='mb-16 md:mb-28'>
+      <div className='mb-2 md:mb-5'>
+        <Image
+          priority
+          src={data.photoUrl}
+          alt={data.photoAlt}
+          width={100}
+          height={100}
+          className='rounded-lg w-[80px] h-[80px] md:w-[100px] md:h-[100px]'
+        />
+      </div>
+      <div>
+        <div className='mb-6 md:mb-10'>
+          <Sparkles>
+            <h1 className='leading-8 text-3xl md:text-5xl font-bold dark:text-white'>
+              {introductionSection.h1}
+            </h1>
+          </Sparkles>
+          <h2 className='text-lg dark:text-gray-300'>
+            {introductionSection.role}
+          </h2>
         </div>
-        <div>
-            <h1 className="leading-8 text-3xl md:text-5xl font-bold dark:text-white">{introductionSection.h1}</h1>
-            <h2 className="mb-6 md:mb-10 text-lg dark:text-gray-300">{introductionSection.role}</h2>
-            <div className="flex flex-col mb-6 md:mb-8 md:flex-row md:items-start">
-                <h3 className="flex items-center gap-2 mb-1 text-sm text-gray-600 dark:text-gray-400 md:min-w-[128px]">
-                    <DescriptionIcon />
-                    About
-                </h3>
-                <p className="dark:text-gray-300">{introductionSection.description}</p>
-            </div>
-            <div className="flex flex-col md:flex-row md:items-start">
-                <h3 className="flex items-center gap-2 mb-1 text-sm text-gray-600 dark:text-gray-400 md:min-w-[128px]">
-                    <MessageIcon />
-                    Contact
-                </h3>
-                <div className="flex flex-wrap gap-4">
-                    {introductionSection.contact.map((item: Contacts) => {
-                        return (
-                            <ButtonBg key={item.contactId} href={item.url} color={ButtonColor.default} text={item.name} />
-                            )   
-                        }
-                    )}
-                </div>
-            </div>
+        <div className='flex flex-col mb-6 md:mb-8 md:flex-row md:items-start'>
+          <h3 className='flex items-center gap-2 mb-1 text-sm text-gray-600 dark:text-gray-400 md:min-w-[128px]'>
+            <DescriptionIcon />
+            About
+          </h3>
+          <p className='dark:text-gray-300'>
+            {introductionSection.description}
+          </p>
         </div>
+        <div className='flex flex-col md:flex-row md:items-start'>
+          <h3 className='flex items-center gap-2 mb-1 text-sm text-gray-600 dark:text-gray-400 md:min-w-[128px]'>
+            <MessageIcon />
+            Contact
+          </h3>
+          <div className='flex flex-wrap gap-4'>
+            {introductionSection.contact.map((item: Contacts) => {
+              return (
+                <ButtonBg
+                  key={item.contactId}
+                  href={item.url}
+                  color={ButtonColor.default}
+                  text={item.name}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
