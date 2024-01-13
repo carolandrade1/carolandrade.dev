4cde16fac0fc23cffc751f5c75a39701f1a62c7b
Carol <carolandrades11@gmail.com>
2024-01-13 13:07:12 -0500

    chore: update info file.

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,223 +1,188 @@
 export const info = {
   '/': {
-    "name": "Home",
-    "photoUrl": "/profilePhoto.jpeg",
-    "photoAlt": "Picture of Carol Andrade",
-    "introductionSection": {
-      "h1": "Carol Andrade Santos",
-      "role": "Front end developer ✨",
-      "description": "Hello 👋 I'm Carol, a Front End Developer from Brazil 🇧🇷. Now I currently live in Canada 🇨🇦 and I'm looking for my first job here.",
-      "contact": [
+    name: 'Home',
+    photoUrl: '/profilePhoto.jpeg',
+    photoAlt: 'Picture of Carol Andrade',
+    introductionSection: {
+      h1: 'Carol Andrade Santos',
+      role: 'Front end developer',
+      description:
+        "Hey there! 👋 I'm Carol, a Developer (and, believe it or not, a Civil Engineer) hailing from the vibrant land of Brazil 🇧🇷. Now, you'll find me embracing the cold in Canada 🇨🇦, where I'm a Junior Developer at MRG Group. Yep, I'm the Brazilian who swapped summer for snowflakes ❄️😆",
+      contact: [
         {
-          "contactId": "01",
-          "name": "Github",
-          "url": "https://github.com/carolandrade1"
+          contactId: '01',
+          name: 'Github',
+          url: 'https://github.com/carolandrade1',
         },
         {
-          "contactId": "02",
-          "name": "LinkedIn",
-          "url": "https://www.linkedin.com/in/carolandrade1/"
+          contactId: '02',
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/carolandrade1/',
         },
         {
-          "contactId": "03",
-          "name": "Codepen",
-          "url": "https://codepen.io/carolandrade1"
-        }
-      ]
+          contactId: '03',
+          name: 'Codepen',
+          url: 'https://codepen.io/carolandrade1',
+        },
+      ],
     },
-    "projectSection": {
-      "h2": "Projects",
-      "description": "Here you can see some of my projects that I did in the past.",
-      "gridProject": [
+    projectSection: {
+      h2: 'Projects',
+      description:
+        'Here you can see some of my projects that I did in the past.',
+      gridProject: [
         {
-          "cardId": "01",
-          "title": "Old Portfolio",
-          "description": "Old portfolio using Styled-components and Next.js",
-          "link": "https://carol-portfolio.vercel.app/",
-          "linkCode": "https://github.com/carolandrade1/Carol_Portfolio",
-          "buttonText": "See live",
-          "buttonAriaText": "See project Old Portfolio Live",
-          "buttonAriaTextCode": "See project Old Portfolio Code"
+          cardId: '01',
+          title: 'Old Portfolio',
+          description: 'Old portfolio using Styled-components and Next.js',
+          link: 'https://carol-portfolio.vercel.app/',
+          linkCode: 'https://github.com/carolandrade1/Carol_Portfolio',
+          buttonText: 'See live',
+          buttonAriaText: 'See project Old Portfolio Live',
+          buttonAriaTextCode: 'See project Old Portfolio Code',
         },
         {
-          "cardId": "02",
-          "title": "Google Clone",
-          "description": "Google clone using Next.js and Tailwind",
-          "link": "https://google-clone-livid-zeta.vercel.app/pt-BR",
-          "linkCode": "https://github.com/carolandrade1/GoogleClone",
-          "buttonText": "See live",
-          "buttonAriaText": "See project Google clone Live",
-          "buttonAriaTextCode": "See project Google clone Code"
+          cardId: '02',
+          title: 'Google Clone',
+          description: 'Google clone using Next.js and Tailwind',
+          link: 'https://google-clone-livid-zeta.vercel.app/pt-BR',
+          linkCode: 'https://github.com/carolandrade1/GoogleClone',
+          buttonText: 'See live',
+          buttonAriaText: 'See project Google clone Live',
+          buttonAriaTextCode: 'See project Google clone Code',
         },
         {
-          "cardId": "03",
-          "title": "Projects JavaScript",
-          "description": "Multiple projects manipulating the DOM",
-          "link": "https://carolandrade1.github.io/Javascript_Projects/",
-          "linkCode": "https://github.com/carolandrade1/Javascript_Projects",
-          "buttonText": "See live",
-          "buttonAriaText": "See Projects JavaScript Live",
-          "buttonAriaTextCode": "See Projects JavaScript Code"
+          cardId: '03',
+          title: 'Projects JavaScript',
+          description: 'Multiple projects manipulating the DOM',
+          link: 'https://carolandrade1.github.io/Javascript_Projects/',
+          linkCode: 'https://github.com/carolandrade1/Javascript_Projects',
+          buttonText: 'See live',
+          buttonAriaText: 'See Projects JavaScript Live',
+          buttonAriaTextCode: 'See Projects JavaScript Code',
         },
         {
-          "cardId": "04",
-          "title": "Responsive Layouts",
-          "description": "10 Modern CSS single-line styling",
-          "link": "https://carolandrade1.github.io/Responsive_Layout/",
-          "linkCode": "https://github.com/carolandrade1/Responsive_Layout",
-          "buttonText": "See live",
-          "buttonAriaText": "See project Responsive Layouts Live",
-          "buttonAriaTextCode": "See project Responsive Layouts Code"
+          cardId: '04',
+          title: 'Responsive Layouts',
+          description: '10 Modern CSS single-line styling',
+          link: 'https://carolandrade1.github.io/Responsive_Layout/',
+          linkCode: 'https://github.com/carolandrade1/Responsive_Layout',
+          buttonText: 'See live',
+          buttonAriaText: 'See project Responsive Layouts Live',
+          buttonAriaTextCode: 'See project Responsive Layouts Code',
         },
         {
-          "cardId": "05",
-          "title": "Instalura",
-          "description": "Clone of Instagram using Next.js, Styled-components and Graphql",
-          "link": "https://instalura-cas.vercel.app/",
-          "linkCode": "https://github.com/carolandrade1/instalura",
-          "buttonText": "See live",
-          "buttonAriaText": "See project Instalura Live",
-          "buttonAriaTextCode": "See project Instalura Code"
-        }
-      ]
+          cardId: '05',
+          title: 'Instalura',
+          description:
+            'Clone of Instagram using Next.js, Styled-components and Graphql',
+          link: 'https://instalura-cas.vercel.app/',
+          linkCode: 'https://github.com/carolandrade1/instalura',
+          buttonText: 'See live',
+          buttonAriaText: 'See project Instalura Live',
+          buttonAriaTextCode: 'See project Instalura Code',
+        },
+      ],
     },
-    "librarySection": {
-      "h2": "Reference Library",
-      "description": "In this list, you will find many wonderful articles/videos that I used to create this portfolio. And as they say, good things have to be shared 😄",
-      "tableRef": [
+    librarySection: {
+      h2: 'Reference Library',
+      description:
+        'In this list, you will find many wonderful articles/videos that I used to create this portfolio. And as they say, good things have to be shared 😄',
+      tableRef: [
         {
-          "id": "1",
-          "title": "Adding a dark/light mode toggle to Next.js using CSS variables",
-          "author": "Rob Morieson",
-          "url": "https://electricanimals.com/articles/next-js-dark-mode-toggle",
-          "subject": [
-            "All",
-            "Accessibility",
-            "Style"
-          ]
+          id: '1',
+          title:
+            'Adding a dark/light mode toggle to Next.js using CSS variables',
+          author: 'Rob Morieson',
+          url: 'https://electricanimals.com/articles/next-js-dark-mode-toggle',
+          subject: ['All', 'Accessibility', 'Style'],
         },
         {
-          "id": "2",
-          "title": "Taillwind Docs",
-          "author": "Taillwind",
-          "url": "https://tailwindcss.com/docs/",
-          "subject": [
-            "All",
-            "Tools"
-          ]
+          id: '2',
+          title: 'Taillwind Docs',
+          author: 'Taillwind',
+          url: 'https://tailwindcss.com/docs/',
+          subject: ['All', 'Tools'],
         },
         {
-          "id": "3",
-          "title": "Thinking about accessibility—a few tips to make a difference",
-          "author": "Aman Singh",
-          "url": "https://amanexplains.com/thinking-about-accessibility-a-few-tips/",
-          "subject": [
-            "All",
-            "Accessibility"
-          ]
+          id: '3',
+          title: 'Thinking about accessibility—a few tips to make a difference',
+          author: 'Aman Singh',
+          url: 'https://amanexplains.com/thinking-about-accessibility-a-few-tips/',
+          subject: ['All', 'Accessibility'],
         },
         {
-          "id": "4",
-          "title": "How to create an accessible Tabs component in React?",
-          "author": "Aman Singh",
-          "url": "https://amanexplains.com/how-to-create-an-accessible-tabs-component-in-react/",
-          "subject": [
-            "All",
-            "Accessibility"
-          ]
+          id: '4',
+          title: 'How to create an accessible Tabs component in React?',
+          author: 'Aman Singh',
+          url: 'https://amanexplains.com/how-to-create-an-accessible-tabs-component-in-react/',
+          subject: ['All', 'Accessibility'],
         },
         {
-          "id": "5",
-          "title": "Supabase Docs",
-          "author": "Supabase",
-          "url": "https://supabase.com/docs/guides/database/api/generating-types",
-          "subject": [
-            "All",
-            "Tools"
-          ]
+          id: '5',
+          title: 'Supabase Docs',
+          author: 'Supabase',
+          url: 'https://supabase.com/docs/guides/database/api/generating-types',
+          subject: ['All', 'Tools'],
         },
         {
-          "id": "6",
-          "title": "Accessible Icon Buttons",
-          "author": "Sara Soueidan",
-          "url": "https://www.sarasoueidan.com/blog/accessible-icon-buttons/",
-          "subject": [
-            "All",
-            "Accessibility"
-          ]
+          id: '6',
+          title: 'Accessible Icon Buttons',
+          author: 'Sara Soueidan',
+          url: 'https://www.sarasoueidan.com/blog/accessible-icon-buttons/',
+          subject: ['All', 'Accessibility'],
         },
         {
-          "id": "7",
-          "title": "TypeScript Docs",
-          "author": "TypeScript",
-          "url": "https://www.typescriptlang.org/docs/handbook/enums.html",
-          "subject": [
-            "All",
-            "Tools"
-          ]
+          id: '7',
+          title: 'TypeScript Docs',
+          author: 'TypeScript',
+          url: 'https://www.typescriptlang.org/docs/handbook/enums.html',
+          subject: ['All', 'Tools'],
         },
         {
-          "id": "8",
-          "title": "Building a Magical 3D Button",
-          "author": "Josh W. Comeau",
-          "url": "https://www.joshwcomeau.com/animation/3d-button/",
-          "subject": [
-            "All",
-            "Style"
-          ]
+          id: '8',
+          title: 'Building a Magical 3D Button',
+          author: 'Josh W. Comeau',
+          url: 'https://www.joshwcomeau.com/animation/3d-button/',
+          subject: ['All', 'Style'],
         },
         {
-          "id": "9",
-          "title": "Part 2: Setting up a Next.js Site for Accessibility",
-          "author": "Alexander Dubovoy",
-          "url": "https://prismic.io/blog/nextjs-accessibility",
-          "subject": [
-            "All",
-            "Accessibility"
-          ]
+          id: '9',
+          title: 'Part 2: Setting up a Next.js Site for Accessibility',
+          author: 'Alexander Dubovoy',
+          url: 'https://prismic.io/blog/nextjs-accessibility',
+          subject: ['All', 'Accessibility'],
         },
         {
-          "id": "10",
-          "title": "Next.js 13 Beta Docs",
-          "author": "Next.js",
-          "url": "https://beta.nextjs.org/docs",
-          "subject": [
-            "All",
-            "Tools"
-          ]
+          id: '10',
+          title: 'Next.js 13 Beta Docs',
+          author: 'Next.js',
+          url: 'https://beta.nextjs.org/docs',
+          subject: ['All', 'Tools'],
         },
         {
-          "id": "11",
-          "title": "The Quest for the Perfect Dark Mode",
-          "author": "Josh W. Comeau",
-          "url": "https://www.joshwcomeau.com/react/dark-mode/",
-          "subject": [
-            "All",
-            "Style"
-          ]
+          id: '11',
+          title: 'The Quest for the Perfect Dark Mode',
+          author: 'Josh W. Comeau',
+          url: 'https://www.joshwcomeau.com/react/dark-mode/',
+          subject: ['All', 'Style'],
         },
         {
-          "id": "12",
-          "title": "Practical CSS and design tips that helps in building future-proof UI.",
-          "author": "Ahmad Shadeed",
-          "url": "https://defensivecss.dev/",
-          "subject": [
-            "All",
-            "Style"
-          ]
+          id: '12',
+          title:
+            'Practical CSS and design tips that helps in building future-proof UI.',
+          author: 'Ahmad Shadeed',
+          url: 'https://defensivecss.dev/',
+          subject: ['All', 'Style'],
         },
         {
-          "id": "13",
-          "title": "Accessibility Cheatsheets for Components",
-          "author": "Sparkbox",
-          "url": "https://accessible-components.sparkbox.com/",
-          "subject": [
-            "All",
-            "Accessibility",
-            "Style"
-          ]
-        }
-      ]
-    }
-  }
+          id: '13',
+          title: 'Accessibility Cheatsheets for Components',
+          author: 'Sparkbox',
+          url: 'https://accessible-components.sparkbox.com/',
+          subject: ['All', 'Accessibility', 'Style'],
+        },
+      ],
+    },
+  },
 };
